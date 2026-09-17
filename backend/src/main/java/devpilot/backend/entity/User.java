a40f4ca8b12a3c98c2233486c706911505afd5d8
starheep <akshatgarg492@gmail.com>
2026-09-17 19:29:39 +0530

    User Entity added

--- backend/src/main/java/devpilot/backend/entity/User.java
+++ backend/src/main/java/devpilot/backend/entity/User.java
@@ -1,4 +1,41 @@
 package devpilot.backend.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    @Column(name = "github_id" , unique = true , nullable = false)
+    private Long githubId;
+    @Column(name = "github_username" , nullable = false , length = 100)
+    private String githubUsername;
+    @Column(name = "display_name" , nullable = false , length = 200)
+    private String displayName;
+    @Column(name = "avatar_url" , length = 500)
+    private String avatarUrl;
+    @Column(name = "access_token" , nullable = false , columnDefinition = "TEXT")
+    private String accessToken;
+    @Column(name = "token_scope" , length = 500)
+    private String tokenScope;
+    @Column(name = "created_at" , nullable = false , updatable = false)
+    private Instant createdAt;
+
+    @PrePersist
+    void onCreate() {
+        if(createdAt == null) {}
+        createdAt = Instant.now();
+    }
+
 }
